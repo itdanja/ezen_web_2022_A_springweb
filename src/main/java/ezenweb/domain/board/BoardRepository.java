e2ba86381afa031ae130bac95f1433ce50a19ad2
itdanja <itdanja@kakao.com>
2022-06-22 18:19:47 +0900

    77일차_검색과 페이징처리

--- src/main/java/ezenweb/domain/board/BoardRepository.java
+++ src/main/java/ezenweb/domain/board/BoardRepository.java
@@ -21,7 +21,7 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
         // 5. [ 직접 쿼리작성 ] :  @Query( value = "쿼리문작성" , nativeQuery = true )
                         // SQL에 변수 넣기
                              // * 필드(column) 명은 변수 으로 불가능
-                            // * @Param ( ) 생략가능
+                            // * @Param ( ) 생략가능   (  JDK 1.8 초과 이면 생략불가능 -> 무조건 사용  )
                            //  *     :변수명      ,    ?인수순서번호
                             // 1.   [인수] @Param("변수명") String 변수명   ->   [SQL ]  :변수명
                             // 2.   [ 인수] @Param("변수명") 엔티티 변수명   --> [ SQL ]   :#{ #엔티티명.필드명 }
@@ -33,7 +33,7 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 //            @Query( value = "select * from board where btitle = :keyword" , nativeQuery = true )
 //            List<BoardEntity> findBybtitle( @Param("keyword") String keyword  );
         @Query( value = "select * from board where cno = :cno and btitle like %:keyword%" , nativeQuery = true )
-        Page<BoardEntity> findBybtitle(int cno , @Param("keyword")  String keyword , Pageable pageable);
+        Page<BoardEntity> findBybtitle( int cno , @Param("keyword")  String keyword , Pageable pageable);
         // List 대신 Page 사용하는이유 : Page 관련된 메소드를 사용하기 위해
 
         // 2. 내용 검색
